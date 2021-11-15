7f637efd67d0a80a26db36fa803f99ebefff66d6
nhattien015 <90367928+nhattien015@users.noreply.github.com>
2021-11-15 20:39:30 +0700

    Update rootSaga.ts

--- src/app/rootSaga.ts
+++ src/app/rootSaga.ts
@@ -1,6 +1,5 @@
 
-import {  } from 'redux-saga';
-import { all, takeEvery } from 'redux-saga/effects';
+import { all } from 'redux-saga/effects';
 import { watchUserAuthSaga } from '../features/user/authSaga';
 import { watchOrderSaga } from '../features/user/orderSaga';
 export default function* rootSaga(){
